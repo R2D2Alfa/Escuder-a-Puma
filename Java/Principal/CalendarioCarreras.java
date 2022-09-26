003c48cd900b52099cafe05f6e7075e2c6b6054a
Erick López González <ericklopezgonzalez602@gmail.com>
2022-09-25 19:47:25 -0500

    Update CalendarioCarreras.java
    Propuesta inicial de métodos de acceso

--- Java/Principal/CalendarioCarreras.java
+++ Java/Principal/CalendarioCarreras.java
@@ -1,10 +1,38 @@
 import java.util.*;
+import java.time.*;
 
 public class CalendarioCarreras {
     String FechaInicio;
     String FechaFinal;
     String Carrera;
 
+    public CalendarioCarreras(String FechaInicio, String FechaFinal, String Carrera){
+        this.FechaInicio = FechaInicio;
+        this.FechaFinal = FechaFinal;
+        this.Carrera = Carrera;
+    }
 
-    
+    public String getFechaInicio(){
+        return FechaInicio;
+    }
+
+    public String getFechaFinal(){
+        return FechaFinal;
+    }
+
+    public String getCarrera(){
+        return Carrera;
+    }
+
+    public void setFechaInicio(){
+
+    }
+
+    public void setFechaFinal(){
+
+    }
+
+    public void setCarrera(){
+
+    }
 }
