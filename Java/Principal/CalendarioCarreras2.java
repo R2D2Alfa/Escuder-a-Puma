66eec350e8b1b1258aab1ba7ec5bc9eb50e818d6
Eyfabricio <95590497+Eyfabricio@users.noreply.github.com>
2022-10-01 17:47:36 -0500

    Create CalendarioCarreras2.java
    Propuesta para el manejo de las fechas

---
+++ Java/Principal/CalendarioCarreras2.java
@@ -0,0 +1,90 @@
+//Esta es una nueva propuesta para el manejo de las fechas :)
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class CalendarioCarreras2 {
+    //Atributos
+    String carrera;
+    LocalDate fechainicio;
+    LocalDate fechafinal;
+    
+
+    //Constructor
+    public CalendarioCarreras2(String carrera, String fechainicio, String fechafinal){
+        //Se convierte las fechas ingresadas
+        LocalDate fechainicio_convert = LocalDate.parse(fechainicio);
+        LocalDate fechafinal_convert = LocalDate.parse(fechafinal);
+
+        //Se asignan las fechas convertidas en conjunto a la carrera
+        this.carrera = carrera;
+        this.fechainicio = fechainicio_convert;
+        this.fechafinal = fechafinal_convert;
+    }
+
+
+    //Getters
+    public String getCarrera() {
+        return carrera;
+    }
+
+
+    public LocalDate getFechainicio(){
+        return fechainicio;
+    }
+
+
+    public LocalDate getFechafinal() {
+        return fechafinal;
+    }
+
+
+    //Setters
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+
+    public void setFechainicio(String fechainicio) {
+        LocalDate fechainicio_convert = LocalDate.parse(fechainicio);
+        this.fechainicio = fechainicio_convert;
+    }
+
+
+    public void setFechafinal(String fechafinal) {
+        LocalDate fechafinal_convert = LocalDate.parse(fechafinal);
+        this.fechafinal = fechafinal_convert;
+    }
+
+
+    //Metodos adicionales
+    public static CalendarioCarreras2 crearCalendarioCarrera(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\n\t[ Creacion del calendario para la carrera ]");
+
+        System.out.print("\nIngrese la carrera de la cual se designara la fecha: ");
+        String carrera = sc.nextLine();
+
+        System.out.print("\nIngrese fecha de inicio de la forma (aaaa-mm-dd)");
+        String inicio = sc.nextLine();
+
+        System.out.print("\nIngrese fecha de fin de la forma (aaaa-mm-dd)");
+        String fin = sc.nextLine();
+
+        CalendarioCarreras2 carrera_calendario = new CalendarioCarreras2(carrera, inicio, fin);
+
+        return carrera_calendario;
+    }
+
+
+    public void verfechacarrera(){
+
+        System.out.println("\n\t[ Fecha de la carrera]");
+
+        System.out.print("\nCarrera: "+this.getCarrera());
+
+        System.out.print("\nLa carrera inicia el: "+this.getFechainicio()+" y termina el: "+this.getFechafinal());
+
+        System.out.print("\n\tBuena suerte a todos los participantes :)");
+    }
+}
