eba8230ac9eafe813897ddef690d01a51a4178a6
Eyfabricio <95590497+Eyfabricio@users.noreply.github.com>
2022-10-02 14:00:08 -0500

    Nuevos cambios

--- Java/Principal/EscuderíaPuma.java
+++ Java/Principal/EscuderíaPuma.java
@@ -1,14 +1,18 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class EscuderíaPuma {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         boolean salir = false, salircamp = false;
+
+
         int principal, campeonato; 
-        String ciudad, pais;
-        float longitud;
         ArrayList<Pistas> pistas = new ArrayList<>();
+        ArrayList<Corredores> corredores = new ArrayList<>();
+
+
         System.out.println("         ################# #####");
         System.out.println("      ################### #####");
         System.out.println("     #####               #####");
@@ -20,55 +24,66 @@ public class EscuderíaPuma {
         System.out.println("Bienvenido al simulador de campeonatos de F1");
         System.out.println("¿Estas cansado de que siempre gane Versttapen o Hamilton?");
         System.out.println("No te preocupes, aquí hasta tu abuela puede ganar (probablemente)");
+        
+        
         while(!salir){
-            System.out.println("Seleccione una opción");
-            System.out.println("1.- Crear un campeonato nuevo");
-            System.out.println("2.- Ver pilotos actuales y campeonatos anteriores");
-            System.out.println("3.- Salir");
+            System.out.println("\n\t[ Seleecione una opcion ]");
+            System.out.println("(1) - Crear un campeonato nuevo");
+            System.out.println("(2) - Ver pilotos actuales y campeonatos anteriores");
+            System.out.println("(3) - Salir");
             principal = sc.nextInt();
+            
+            
             switch (principal){
                 case 1:
                     while(!salircamp){
-                        System.out.println("Seleccione una opción");
-                        System.out.println("1.- Empezar campeonato");
-                        System.out.println("2.- Crear corredores");
-                        System.out.println("3.- Crear pistas");
-                        System.out.println("4.- Salir");
+                        System.out.println("\n\t[ Seleecione una opcion ]");
+                        System.out.println("(1) - Empezar campeonato");
+                        System.out.println("(2) - Crear corredores");
+                        System.out.println("(3) - Crear pistas");
+                        System.out.println("(4) - Salir");
                         campeonato = sc.nextInt();
+                        
+                        
                         switch (campeonato){
                             case 1:
+                                System.out.println("\n\t[ Empezar campeonato ]");
                                 
                             break;
+
+
                             case 2:
-
+                                System.out.println("\n\t[ Crear corredores ]");
+                                Corredores corredor = Corredores.crearCorredor();
+                                corredores.add(corredor);
                             break;
+
+
                             case 3:
-                                System.out.println("Ingrese el país donde se desempeñará la carrera");
-                                pais = sc.nextLine();
-                                System.out.println("Ingrese la ciudad cede de la carrera");
-                                ciudad = sc.nextLine();
-                                System.out.println("Ingrese el procesador de la computadora");
-                                longitud = sc.nextFloat();
-                                Pistas nuevapista = new Pistas(pais, ciudad, longitud);
-                                pistas.add(nuevapista);
+                                System.out.println("\n\t[ Crear pistas ]");
+                                Pistas nueva_pista = Pistas.crearPista();
+                                pistas.add(nueva_pista);
                             break;
-                            case 4:
 
+
+                            case 4:
+                                salircamp = true;
                             break;
                         }
                     }
                 break;
+
+
                 case 2:
-                    
+                    System.out.println("\n\t[ Ver pilotos actuales y campeonatos anteriores ]");
                 break;
+
+
                 case 3:
+                    System.out.println("\n\tHasta luego :)");
                     salir = true;
                 break;
             }
         }
-            
-
-
     }
-
 }
--- Java/Principal/Iniciar.java
+++ Java/Principal/Iniciar.java
@@ -1,180 +1,218 @@
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedList;
 
 
 public class Iniciar {
+
+
     public static void iniciarPistas(){
         ArrayList<Pistas> pistas = new ArrayList<>();
         //Creación de pistas
+        
         //1. Bahrein
             Pistas Bahrein = new Pistas("Bahrein", "Sakhir", 5412f);
             pistas.add(Bahrein);
+
+
         //2. Arabia Saudita
             Pistas Arabia = new Pistas("Arabia Saudita", "Yeda", 6175f);
             pistas.add(Arabia);
+
+
         //3. Australia
             Pistas Australia = new Pistas("Australia", "Melbourne", 5303f);
             pistas.add(Australia);
+
+
         //4. Italia
             Pistas Emilia = new Pistas("Italia", "Imola", 4908f);
             pistas.add(Emilia);
+
+
         //5. Estados Unidos
             Pistas Miami = new Pistas("Estados Unidos", "Miami", 5410f);
             pistas.add(Miami);
+
+
         //6. España
             Pistas España = new Pistas("España", "Barcelona", 5655f);
             pistas.add(España);
+
+
         //7. Mónaco
             Pistas Monaco = new Pistas("Monaco", "Monaco", 4655f);
             pistas.add(Monaco);
+
+
         //8. Azerbaiyán
             Pistas Azerbaiyan = new Pistas("Azerbaiyan", "Baku", 6003f);
             pistas.add(Azerbaiyan);
+
+
         //9. Canada
             Pistas Canada = new Pistas("Canada", "Montreal", 4361f);
             pistas.add(Canada);
+
+
         //10. Gran Bretaña
             Pistas Gb = new Pistas("Gran Bretaña", "Silverstone", 5891f);
             pistas.add(Gb);
+
+
         //11. Austria
             Pistas Austria = new Pistas("Austria", "Knittelfeld", 4326f);
             pistas.add(Austria);
+
+
         //12. Francia
             Pistas Francia = new Pistas("Francia", "Le Castellet", 5842f);
             pistas.add(Francia);
+
+
         //13. Hungria
             Pistas Hungria = new Pistas("Hungria", "Budapest", 4381f);
             pistas.add(Hungria);
+
+
         //14. Bélgica
             Pistas Belgica = new Pistas("Belgica", "Spa-Francorchamps", 7000.4f);
             pistas.add(Belgica);
+
+
         //15. Paises Bajos
             Pistas Holanda = new Pistas("Paises Bajos", "Zandvoort", 4259f);
             pistas.add(Holanda);
+
+
         //16. Italia
             Pistas Italia = new Pistas("Italia", "Monza", 5793f);
             pistas.add(Italia);
+
+
         //17. Singapur
             Pistas Singapur = new Pistas("Singapur", "ingapur", 5073f);
             pistas.add(Singapur);
+
+
         //18. Japon
             Pistas Japon = new Pistas("Japon", "Suzaka", 5807f);
             pistas.add(Japon);
+
+
         //19. Estados Unidos
             Pistas Eu = new Pistas("Estados Unidos", "Austin", 5513f);
             pistas.add(Eu);
+
+
         //20. Mexico
             Pistas Mexico = new Pistas("Mexico", "Ciudad de Mexico", 4421f);
             pistas.add(Mexico);
+
+
         //21. Brasil
             Pistas Brasil = new Pistas("Brasil", "Sao Paulo", 4309f);
             pistas.add(Brasil);
+
+
         //22. Abu Dhabi
             Pistas Ea = new Pistas("Emiratos Arabes Unidos", "Abu Dhabi", 5524f);
             pistas.add(Ea);
     }
 
-    public static void iniciarCorredores(){
+
+    public static void iniciarEscuderias(){
 
         //Creación de corredores por escuderias
         
         //Escuderia Williams
-        HashSet<Corredores> escuderia_williams = new HashSet<Corredores>();
+        LinkedList<Corredores> corredores_williams = new LinkedList<Corredores>();
         
         Corredores williams_1 = new Corredores("Paises Bajos", "Nyck", "Vries", 45, 75, 1.74f);
         Corredores williams_2 = new Corredores("Tailandia", "Alexander", "Albon", 23, 74, 1.86f);
         
-        escuderia_williams.add(williams_1);
-        escuderia_williams.add(williams_2);
+        Escuderias escuderia_williams = new Escuderias("Williams", corredores_williams);
+
 
 
         //Escuderia Mercedes
-        HashSet<Corredores> escuderia_mercedes = new HashSet<Corredores>();
+        LinkedList<Corredores> corredores_mercedes = new LinkedList<Corredores>();
 
         Corredores mercedes_1 = new Corredores("Gran Bretaña", "Lewis","Hamilton", 44, 68, 1.74f);
         Corredores mercedes_2 = new Corredores("Gran Bretaña", "George","Rusell", 63, 70, 1.85f);
 
-        escuderia_mercedes.add(mercedes_1);
-        escuderia_mercedes.add(mercedes_2);
+        Escuderias escuderia_mercedes = new Escuderias("Mercedes", corredores_mercedes);
 
 
         //Escuderia Red Bull
-        HashSet<Corredores> escuderia_redbull = new HashSet<Corredores>();
+        LinkedList<Corredores> corredores_redbull = new LinkedList<Corredores>();
 
         Corredores redbull_1 = new Corredores("Paises Bajos", "Max", "Verstappen", 1, 67, 1.80f);
         Corredores redbull_2 = new Corredores("Mexico", "Sergio", "Pérez", 11, 64, 1.73f);
 
-        escuderia_redbull.add(redbull_1);
-        escuderia_redbull.add(redbull_2);
+        Escuderias escuderia_redbull = new Escuderias("Redbull", corredores_redbull);
 
 
         //Escuderia Ferrari
-        HashSet<Corredores> escuderia_ferrari = new HashSet<Corredores>();
+        LinkedList<Corredores> corredores_ferrari = new LinkedList<Corredores>();
 
         Corredores ferrari_1 = new Corredores("Monaco", "Charles", "Leclerc", 16, 65, 1.80f);
         Corredores ferrari_2 = new Corredores("España", "Carlos","Sainz", 55, 66, 1.77f);
 
-        escuderia_ferrari.add(ferrari_1);
-        escuderia_ferrari.add(ferrari_2);
+        Escuderias escuderia_ferrari = new Escuderias("Ferrari", corredores_ferrari);
 
 
         //Escuderia McLaren
-        HashSet<Corredores> escuderia_mclaren = new HashSet<Corredores>();
+        LinkedList<Corredores> corredores_mclaren = new LinkedList<Corredores>();
 
         Corredores mclaren_1 = new Corredores("Gran Bretaña", "Lando", "Norris", 4, 68, 1.70f);
         Corredores mclaren_2 = new Corredores("Australia", "Daniel", "Ricciardo", 3, 71, 1.80f);
 
-        escuderia_mclaren.add(mclaren_1);
-        escuderia_mclaren.add(mclaren_2);
+        Escuderias escuderia_mclaren = new Escuderias("Mclaren", corredores_mclaren);
 
 
         //Escuderia Alpine
-        HashSet<Corredores> escuderia_alpine = new HashSet<Corredores>();
+        LinkedList<Corredores> corredores_alpine = new LinkedList<Corredores>();
 
         Corredores alpine_1 = new Corredores("Francia", "Esteban", "Ocon", 31, 66, 1.86f);
         Corredores alpine_2 = new Corredores("España", "Fernando", "Alonso", 14, 68, 1.71f);
 
-        escuderia_alpine.add(alpine_1);
-        escuderia_alpine.add(alpine_2);
+        Escuderias escuderia_alpine = new Escuderias("Alpine", corredores_alpine);
 
 
         //Escuderia Alpha Tauri
-        HashSet<Corredores> escuderia_alphatauri = new HashSet<Corredores>();
+        LinkedList<Corredores> corredores_alphatauri = new LinkedList<Corredores>();
 
         Corredores alphatauri_1 = new Corredores("Francia", "Pierre", "Gasly", 10, 70, 1.77f);
         Corredores alphatauri_2 = new Corredores("Japon", "Yuki", "Tsunoda", 22, 54, 1.59f);
 
-        escuderia_alphatauri.add(alphatauri_1);
-        escuderia_alphatauri.add(alphatauri_2);
+        Escuderias escuderia_alphatauri = new Escuderias("AlphaTauri", corredores_alphatauri);
 
 
         //Escuderia Aston Martin
-        HashSet<Corredores> escuderia_astonmartin = new HashSet<Corredores>();
+        LinkedList<Corredores> corredores_astonmartin = new LinkedList<Corredores>();
         
         Corredores astonmartin_1 = new Corredores("Canada", "Lance", "Stroll", 18, 70, 1.82f);
         Corredores astonmartin_2 = new Corredores("Alemania", "Sebastian", "Vettel", 5, 64, 1.74f);
 
-        escuderia_astonmartin.add(astonmartin_1);
-        escuderia_astonmartin.add(astonmartin_2);
+        Escuderias escuderia_astonmartin = new Escuderias("Aston Martin", corredores_astonmartin);
 
 
         //Escuderia Alfa Romeo
-        HashSet<Corredores> escuderia_alfaromeo = new HashSet<Corredores>();
+        LinkedList<Corredores> corredores_alfaromeo = new LinkedList<Corredores>();
 
         Corredores alfaromeo_1 = new Corredores("Finlandia", "Valtteri", "Bottas", 77, 70, 1.73f);
         Corredores alfaromeo_2 = new Corredores("China", "Guanyu", "Zhou", 24, 63, 1.75f);
 
-        escuderia_alfaromeo.add(alfaromeo_1);
-        escuderia_alfaromeo.add(alfaromeo_2);
+        Escuderias escuderia_alfaromeo = new Escuderias("Alfa Romeo", corredores_alfaromeo);
 
 
         //Escuderia Haas
-        HashSet<Corredores> escuderia_haas = new HashSet<Corredores>();
+        LinkedList<Corredores> corredores_haas = new LinkedList<Corredores>();
 
         Corredores haas_1 = new Corredores("Alemania", "Mick", "Schumacher", 47, 67, 1.76f);
         Corredores haas_2 = new Corredores("Dinamarca", "Kevin", "Magnussen", 20, 68, 1.74f);
 
-        escuderia_haas.add(haas_1);
-        escuderia_haas.add(haas_2);
+        Escuderias escuderia_haas = new Escuderias("Haas", corredores_haas);
     }
 }
