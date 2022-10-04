7cdce71ab36c09be39aee33fced79a7f7258e75f
r2d2Alfa <arturo.ad2203@comunidad.unam.mx>
2022-10-04 12:46:43 -0500

    Escuderias arregldas
    scuderias

--- Java/Principal/Iniciar.java
+++ Java/Principal/Iniciar.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.LinkedList;
 
 
@@ -123,6 +122,9 @@ public class Iniciar {
 
     public static void iniciarEscuderias(){
 
+        //Lista que contendra a las escuderias con sus corredores
+        LinkedList<Escuderias> escuderiasf1 = new LinkedList<Escuderias>();
+
         //Creación de corredores por escuderias
         
         //Escuderia Williams
@@ -130,10 +132,14 @@ public class Iniciar {
         
         Corredores williams_1 = new Corredores("Paises Bajos", "Nyck", "Vries", 45, 75, 1.74f);
         Corredores williams_2 = new Corredores("Tailandia", "Alexander", "Albon", 23, 74, 1.86f);
+
+        corredores_williams.add(williams_1);
+        corredores_williams.add(williams_2);
         
         Escuderias escuderia_williams = new Escuderias("Williams", corredores_williams);
-
-
+        
+        escuderiasf1.add(escuderia_williams);
+        
 
         //Escuderia Mercedes
         LinkedList<Corredores> corredores_mercedes = new LinkedList<Corredores>();
@@ -141,7 +147,12 @@ public class Iniciar {
         Corredores mercedes_1 = new Corredores("Gran Bretaña", "Lewis","Hamilton", 44, 68, 1.74f);
         Corredores mercedes_2 = new Corredores("Gran Bretaña", "George","Rusell", 63, 70, 1.85f);
 
+        corredores_mercedes.add(mercedes_1);
+        corredores_mercedes.add(mercedes_2);
+
         Escuderias escuderia_mercedes = new Escuderias("Mercedes", corredores_mercedes);
+
+        escuderiasf1.add(escuderia_mercedes);
 
 
         //Escuderia Red Bull
@@ -150,7 +161,12 @@ public class Iniciar {
         Corredores redbull_1 = new Corredores("Paises Bajos", "Max", "Verstappen", 1, 67, 1.80f);
         Corredores redbull_2 = new Corredores("Mexico", "Sergio", "Pérez", 11, 64, 1.73f);
 
+        corredores_redbull.add(redbull_1);
+        corredores_redbull.add(redbull_2);
+
         Escuderias escuderia_redbull = new Escuderias("Redbull", corredores_redbull);
+
+        escuderiasf1.add(escuderia_redbull);
 
 
         //Escuderia Ferrari
@@ -159,7 +175,12 @@ public class Iniciar {
         Corredores ferrari_1 = new Corredores("Monaco", "Charles", "Leclerc", 16, 65, 1.80f);
         Corredores ferrari_2 = new Corredores("España", "Carlos","Sainz", 55, 66, 1.77f);
 
+        corredores_ferrari.add(ferrari_1);
+        corredores_ferrari.add(ferrari_2);
+
         Escuderias escuderia_ferrari = new Escuderias("Ferrari", corredores_ferrari);
+
+        escuderiasf1.add(escuderia_ferrari);
 
 
         //Escuderia McLaren
@@ -168,7 +189,12 @@ public class Iniciar {
         Corredores mclaren_1 = new Corredores("Gran Bretaña", "Lando", "Norris", 4, 68, 1.70f);
         Corredores mclaren_2 = new Corredores("Australia", "Daniel", "Ricciardo", 3, 71, 1.80f);
 
+        corredores_mclaren.add(mclaren_1);
+        corredores_mclaren.add(mclaren_2);
+
         Escuderias escuderia_mclaren = new Escuderias("Mclaren", corredores_mclaren);
+
+        escuderiasf1.add(escuderia_mclaren);
 
 
         //Escuderia Alpine
@@ -177,7 +203,12 @@ public class Iniciar {
         Corredores alpine_1 = new Corredores("Francia", "Esteban", "Ocon", 31, 66, 1.86f);
         Corredores alpine_2 = new Corredores("España", "Fernando", "Alonso", 14, 68, 1.71f);
 
+        corredores_alpine.add(alpine_1);
+        corredores_alpine.add(alpine_2);
+
         Escuderias escuderia_alpine = new Escuderias("Alpine", corredores_alpine);
+
+        escuderiasf1.add(escuderia_alpine);
 
 
         //Escuderia Alpha Tauri
@@ -186,7 +217,12 @@ public class Iniciar {
         Corredores alphatauri_1 = new Corredores("Francia", "Pierre", "Gasly", 10, 70, 1.77f);
         Corredores alphatauri_2 = new Corredores("Japon", "Yuki", "Tsunoda", 22, 54, 1.59f);
 
+        corredores_alphatauri.add(alphatauri_1);
+        corredores_alphatauri.add(alphatauri_2);
+
         Escuderias escuderia_alphatauri = new Escuderias("AlphaTauri", corredores_alphatauri);
+
+        escuderiasf1.add(escuderia_alphatauri);
 
 
         //Escuderia Aston Martin
@@ -195,7 +231,12 @@ public class Iniciar {
         Corredores astonmartin_1 = new Corredores("Canada", "Lance", "Stroll", 18, 70, 1.82f);
         Corredores astonmartin_2 = new Corredores("Alemania", "Sebastian", "Vettel", 5, 64, 1.74f);
 
+        corredores_astonmartin.add(astonmartin_1);
+        corredores_astonmartin.add(astonmartin_2);
+
         Escuderias escuderia_astonmartin = new Escuderias("Aston Martin", corredores_astonmartin);
+
+        escuderiasf1.add(escuderia_astonmartin);
 
 
         //Escuderia Alfa Romeo
@@ -204,7 +245,12 @@ public class Iniciar {
         Corredores alfaromeo_1 = new Corredores("Finlandia", "Valtteri", "Bottas", 77, 70, 1.73f);
         Corredores alfaromeo_2 = new Corredores("China", "Guanyu", "Zhou", 24, 63, 1.75f);
 
+        corredores_alfaromeo.add(alfaromeo_1);
+        corredores_alfaromeo.add(alfaromeo_2);
+
         Escuderias escuderia_alfaromeo = new Escuderias("Alfa Romeo", corredores_alfaromeo);
+
+        escuderiasf1.add(escuderia_alfaromeo);
 
 
         //Escuderia Haas
@@ -213,6 +259,15 @@ public class Iniciar {
         Corredores haas_1 = new Corredores("Alemania", "Mick", "Schumacher", 47, 67, 1.76f);
         Corredores haas_2 = new Corredores("Dinamarca", "Kevin", "Magnussen", 20, 68, 1.74f);
 
+        corredores_haas.add(haas_1);
+        corredores_haas.add(haas_2);
+
         Escuderias escuderia_haas = new Escuderias("Haas", corredores_haas);
+
+        escuderiasf1.add(escuderia_haas);
+    }
+
+    public static void iniciarCarreras(){
+
     }
 }
